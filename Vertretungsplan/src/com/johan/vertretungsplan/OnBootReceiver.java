f3d1d4fb64edb83800ef3196ff1cd0a43e3585b6
Johan <johan.forstner@gmail.com>
2013-04-01 13:01:39 +0200

    Version 1.0beta15: Automatische Aktualisierung funktioniert wieder
    Former-commit-id: 209fa592e9f30bdf4e32e88a7634dc61ea53911d

--- Vertretungsplan/src/com/johan/vertretungsplan/OnBootReceiver.java
+++ Vertretungsplan/src/com/johan/vertretungsplan/OnBootReceiver.java
@@ -3,11 +3,13 @@ package com.johan.vertretungsplan;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 
 public class OnBootReceiver extends BroadcastReceiver {
 
 	@Override
 	public void onReceive(Context context, Intent intent) {
+		Log.d("Vertretungsplan", "OnBootReceiver gestartet");
 		Intent autostartIntent = new Intent(context, AutostartService.class);
 		context.startService(autostartIntent);
     }
