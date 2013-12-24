470ef63988f7d8b3621bce53a3a9731512809550
Johan <johan.forstner@gmail.com>
2013-12-24 15:05:36 +0100

    Lizenz und Readme hinzugefügt, Widget funktioniert wieder
    Former-commit-id: d8e80ea845dfbf811ff58140d62b1040d56355d7

---
+++ README.md
@@ -0,0 +1,4 @@
+ls-vertretungsplan
+==================
+
+Dies ist eine Android-App für den Vertretungsplan der Lornsenschule Schleswig. Weitere Informationen aut der [Intermetseite](http://johan12345.github.io/ls-vertretungsplan/)
--- Vertretungsplan/src/com/johan/vertretungsplan/SettingsFragment.java
+++ Vertretungsplan/src/com/johan/vertretungsplan/SettingsFragment.java
@@ -1,3 +1,19 @@
+/*  LS Vertretungsplan - Android-App für den Vertretungsplan der Lornsenschule Schleswig
+    Copyright (C) 2013  Johan v. Forstner
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see [http://www.gnu.org/licenses/]. */
+
 package com.johan.vertretungsplan;
 
 import org.holoeverywhere.preference.PreferenceFragment;
